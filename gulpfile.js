b7971c932cfae03ddef872854aa79ee94e3f5f08
kBohdan <bkachmar1@gmail.com>
2023-04-29 21:42:20 +0300

    create header

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 const gulp = require('gulp');
-const less = require('less');
 const del = require('del');
 const gulpPug = require('gulp-pug');
 const htmlmin = require('gulp-htmlmin');
 const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
-const babel = require('gulp-babel');
 const jsmin = require('gulp-jsmin');
 const webp = require('gulp-webp');
 const browserSync = require("browser-sync").create();
@@ -21,16 +19,12 @@ const paths = {
         dist: 'dist/css'
     },
     scripts: {
-        src: 'src/scripts/**/*.js',
+        src: 'src/js/**/*.js',
         dist: 'dist/js/'
     },
     img: {
         src: 'src/img/**/*.{png,svg,jpeg}',
-        dist: 'dist/data/'
-    },
-    data: {
-        src: 'src/data/**/*.json',
-        dist: 'dist/data/'
+        dist: 'dist/img/'
     }
 }
 
@@ -60,11 +54,6 @@ function styles() {
 function scripts() {
     return gulp
         .src(paths.scripts.src)
-        .pipe(
-            babel({
-                presets: ["@babel/env"],
-            })
-        )
         .pipe(jsmin())
         .pipe(gulp.dest(paths.scripts.dist))
         .pipe(browserSync.stream());
@@ -78,13 +67,6 @@ function images() {
         .pipe(browserSync.stream());
 }
 
-function data() {
-    return gulp
-        .src(paths.data.src)
-        .pipe(gulp.dest(paths.data.dist))
-        .pipe(browserSync.stream());
-}
-
 function watch() {
     browserSync.init({
         server: {
@@ -95,13 +77,12 @@ function watch() {
     gulp.watch(paths.styles.src, styles);
     gulp.watch(paths.scripts.src, scripts);
     gulp.watch(paths.img.src, images);
-    gulp.watch(paths.data.src, data);
 }
 
 const build = gulp.series(
     clean,
     pug,
-    gulp.parallel(styles, scripts, images, data),
+    gulp.parallel(styles, scripts, images),
     watch
 );
 
@@ -110,5 +91,4 @@ exports.pug = pug;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
-exports.data = data;
 exports.default = build;
