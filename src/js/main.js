be9291e4a23efe6a5a1d0b300f74418e2ae16d1c
kBohdan <bkachmar1@gmail.com>
2023-05-01 01:15:02 +0300

    add readme file

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,4 @@ import './module/btn-hover.js';
 import './module/form.js';
 import './module/modal.js';
 import './module/open-box.js';
-
-AOS.init({
-    disable: false,
-    startEvent: 'DOMContentLoaded',
-    initClassName: 'aos-init',
-    animatedClassName: 'aos-animate',
-    useClassNames: false,
-    disableMutationObserver: false,
-    debounceDelay: 50,
-    throttleDelay: 99,
-
-    offset: 120,
-    delay: 0,
-    duration: 600,
-    easing: 'ease',
-    once: false,
-    mirror: false,
-    anchorPlacement: 'top-bottom',
-});
+import './module/aos.js';
